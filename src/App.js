b23bf6d3f66f9f2309f9ffcd0c7829aee1caee3e
thebhumilsoni <thebhumilsoni@gmail.com>
2021-08-17 03:48:55 +1000

    Setup custom domain name for the portfolio

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>I am Bhumil Soni. Welcome to my Portfolio!</h1>
+        <h2>Now this portfolio is hosted on custome domain : www.thebhumilsoni.com</h2>
       </header>
     </div>
   );
