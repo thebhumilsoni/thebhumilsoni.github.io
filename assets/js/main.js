518898113307ed4a52a2929186f8b90879474d8c
Bhumil Soni <48656462+thebhumilsoni@users.noreply.github.com>
2023-12-21 02:49:39 +1100

    Update styles to follow fluid/adaptable layout and mobile-first development approach

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,18 @@
-// Function to handle the transition effect
-const applyTransitionEffect = (element) => {
-  element.style.transition = 'opacity 0.5s ease, visibility 0.5s ease';
-};
-
-// Function to remove the transition effect
-const removeTransitionEffect = (element) => {
-  element.style.transition = 'none';
-};
-
-// Toggle Mobile Navigation
 const toggleMobileNav = () => {
   const navbarLinks = document.querySelector('.navbar-links');
   const navbarToggler = document.querySelector('.navbar-toggler');
   const navbarTogglerIcon = navbarToggler.querySelector('i');
 
-  // Apply or remove the transition effect depending on the open state
-  if (navbarLinks.classList.contains('open')) {
-    removeTransitionEffect(navbarLinks); // This will not remove the rotation effect on the icon
-  } else {
-    applyTransitionEffect(navbarLinks); // Apply the fade transition to the links
-  }
-
   navbarLinks.classList.toggle('open');
-  navbarToggler.classList.toggle('open'); // This line should handle the rotation of the icon
+  navbarToggler.classList.toggle('open');
   navbarTogglerIcon.className = navbarLinks.classList.contains('open') ? 'fas fa-times' : 'fas fa-bars';
 };
 
-
-// Update Theme Icon based on the current theme
 const updateThemeIcon = (isDarkMode) => {
   const themeIcon = document.querySelector('.theme-toggle .theme-icon');
   themeIcon.className = `theme-icon fas ${isDarkMode ? 'fa-sun' : 'fa-moon'}`;
 };
 
-// Initialize Theme Toggle Functionality
 const initializeThemeToggle = () => {
   const themeToggle = document.querySelector('.theme-toggle');
   themeToggle.addEventListener('click', () => {
@@ -43,7 +22,6 @@ const initializeThemeToggle = () => {
   });
 };
 
-// Apply Saved Theme Preference
 const applySavedTheme = () => {
   const savedTheme = localStorage.getItem('theme');
   const isDarkMode = savedTheme === 'dark-theme';
@@ -53,7 +31,6 @@ const applySavedTheme = () => {
   updateThemeIcon(isDarkMode);
 };
 
-// DOM Content Loaded Listener
 document.addEventListener('DOMContentLoaded', () => {
   applySavedTheme();
   initializeThemeToggle();
@@ -61,13 +38,16 @@ document.addEventListener('DOMContentLoaded', () => {
   navbarToggler.addEventListener('click', toggleMobileNav);
 });
 
-// Window Resize Listener for Responsive Design
 window.addEventListener('resize', () => {
   const navbarLinks = document.querySelector('.navbar-links');
+  const navbarTogglerIcon = document.querySelector('.navbar-toggler i');
 
   if (window.innerWidth >= 768) {
-    navbarLinks.classList.remove('open'); // Ensure that the navbar links are not open when resizing to a wider view
-    navbarLinks.style.opacity = '';
-    navbarLinks.style.visibility = '';
+      if (navbarLinks.classList.contains('open')) {
+          navbarLinks.classList.remove('open');
+          navbarTogglerIcon.className = 'fas fa-bars';
+      }
+  } else {
+      navbarTogglerIcon.className = navbarLinks.classList.contains('open') ? 'fas fa-times' : 'fas fa-bars';
   }
 });
