2aaeb89a7c3bce7cc10d50a65c68c3cd4ab5e14b
Bhumil Soni <48656462+thebhumilsoni@users.noreply.github.com>
2023-12-26 01:13:43 +1100

    Add social media shortcuts on nav bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,8 +43,20 @@ document.addEventListener('DOMContentLoaded', () => {
   applySavedTheme();
   initializeThemeToggle();
   adjustContentWrapperPadding();
+
   const navbarToggler = document.querySelector('.navbar-toggler');
   navbarToggler.addEventListener('click', toggleMobileNav);
+
+  const githubButton = document.querySelector('.social-icon.github');
+  const linkedinButton = document.querySelector('.social-icon.linkedin');
+
+  githubButton.addEventListener('click', () => {
+      window.open('https://github.com/thebhumilsoni', '_blank');
+  });
+
+  linkedinButton.addEventListener('click', () => {
+      window.open('https://www.linkedin.com/in/thebhumilsoni', '_blank');
+  });
 });
 
 window.addEventListener('resize', () => {
