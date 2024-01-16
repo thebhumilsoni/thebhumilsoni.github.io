7e1b0eee433b7c2239c76a7f5f3abdfb155bd0fd
Bhumil Soni <48656462+thebhumilsoni@users.noreply.github.com>
2024-01-16 23:06:24 +1100

    Add simple initial particle.js dynamic background

---
+++ assets/js/dynamic-background.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    particlesJS('dynamic-background', {
+        "particles": {
+            "number": {
+                "value": 50
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "shape": {
+                "type": "circle"
+            },
+            "opacity": {
+                "value": 0.5
+            },
+            "size": {
+                "value": 3
+            },
+            "line_linked": {
+                "enable": true,
+                "color": "#ffffff"
+            },
+            "move": {
+                "enable": true,
+                "speed": 3
+            }
+        }
+    });
+});
